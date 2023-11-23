9e507adabf03c19b8349b0a41ecfd9485f91ab73
Preetham B R <90082686+PreethamBR@users.noreply.github.com>
2023-11-23 21:02:31 +0530

    Add files via upload

---
+++ student-attendance-management-system-main/inc/footer.php
@@ -0,0 +1,16 @@
+	<div class="container mt-3">
+		<div class="card card-body bg-light text-center">
+			<h3>
+				<span>Copyright &copy;</span>
+				<script type="text/javascript">
+					var d = new Date();
+					document.write(d.getFullYear());
+				</script>
+						Designed and Developed by <span> Mischief coders.co</span><br>
+						All rights reserved
+					</a>
+			</h3>
+		</div>
+	</div>
+</body>
+</html>
